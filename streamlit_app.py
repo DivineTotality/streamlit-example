c3dcbd0ae5a704c5761df1276fa20fcb275e3ce1
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-05-27 01:13:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,79 @@
 import streamlit as st
+from random import randint
+from importlib import reload
+from os import system
+from time import sleep
+l = 0
+pts = 0
+lib = 0
+
+def Writing():
+  UserInput = input("Code (Use ; for Breakline): ")
+  f = open("TrueUserInput.py", "w")
+  f.write(UserInput)
+  f.close()
+
+
+def HighScoreRecord():
+  global pts
+  HSR = open("HighScore.txt", "r")
+  InterPTS = HSR.read()
+  InterPTS = int(InterPTS)
+  HSR.close()
+  print('Current High Score:', InterPTS, "\n")
+  
+  if InterPTS <= pts:
+    HS = open("HighScore.txt", "w")
+    StringPTS = str(pts)
+    HS.write(StringPTS)
+    HS.close()
+  else:
+    return 0
+
+def Checker():
+  global lib
+  global pts
+  global TrueUserInput
+  if realOutput == 1:
+    Question1 = (randint(1,1000))
+    print("output the value of x as:", Question1)
+    Writing()
+    
+  elif realOutput == 2:
+    QuestionInput =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
+    print("Make a program that combines all these numbers,saved as x, y, z:", QuestionInput)
+    Writing()
+
+  sleep(0.5)
+  if lib==0:
+    import TrueUserInput
+    lib=1
+  else:
+    reload(TrueUserInput)
+
+  if realOutput == 1:
+    if TrueUserInput.x == Question1:
+      print("\nCorrect!\n")
+      print("P:", pts, "+ 100")
+      pts += 100
+      sleep(1)
+    else:
+      print("\nIncorrect!\n")
+      print("P:", pts)
+      sleep(1)
+  elif realOutput==2:
+    if TrueUserInput.x+TrueUserInput.y+TrueUserInput.z == QuestionInput[0]+QuestionInput[1]+QuestionInput[2]:
+      print("\nCorrect\n")
+      print("P:", pts, "+ 150")
+      pts += 150
+      sleep(1)
+    else:
+      print("\nIncorrect!\n")
+      print("P:", pts)
+      sleep(1) 
 
 
 st.title("Code-It-Out!!")
-UserInput = st.text_input("Code: ")
+UserInput = st.text_area("Code: ")
 st.code(UserInput)
 
