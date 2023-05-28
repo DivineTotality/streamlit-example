5f2feff9ec8657c57e105066a227c6b03b29a9a8
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-05-28 22:04:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,8 +75,7 @@ def Checker():
       print("P:", pts)
       sleep(1) 
 
-
-st.title("Code-It-Out!!")
-Writing()
 with st.echo():
   st.write('Code will be executed and printed')
+  st.title("Code-It-Out!!")
+  Writing()
