e7ac6c609c53c6428dfcf92effb175b835326f49
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-06-07 12:35:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,81 +1,78 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 from random import randint
-from importlib import reload
-from os import system
 from time import sleep
+
 realOutput = (randint(1,2))
 l = 0
 pts = 0
 lib = 0
 
-def Writing():
-  TrueUserInput = st.text_area("Code: ")
-  if st.button("Submit"):
-    st.code(TrueUserInput)
-    with open('TrueUserInput.py', 'w') as file_obj: # open function with 'w' argument it is mean you will add some text in empty file
-      file_obj.write(str(TrueUserInput))
+class SessionState:
+    def __init__(self):
+        self.TrueUserInput = ""
 
+state = SessionState()
+
+def Writing():
+    state.TrueUserInput = st.text_area("Code: ")
+    if st.button("Submit"):
+        st.code(state.TrueUserInput)
 
 def HighScoreRecord():
-  global pts
-  HSR = open("HighScore.txt", "r")
-  InterPTS = HSR.read()
-  InterPTS = int(InterPTS)
-  HSR.close()
-  print('Current High Score:', InterPTS, "\n")
-  
-  if InterPTS <= pts:
-    HS = open("HighScore.txt", "w")
-    StringPTS = str(pts)
-    HS.write(StringPTS)
-    HS.close()
-  else:
-    return 0
-  
-def Question():
-  global lib
-  global pts
-  global TrueUserInput
-  if realOutput == 1:
-    Question1 = (randint(1,1000))
-    st.write("output the value of x as:", Question1)
-    Writing()
-    
-  elif realOutput == 2:
-    QuestionInput =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
-    st.write("Make a program that combines all these numbers, saved as x, y, z:", QuestionInput)
-    Writing()
+    global pts
+    HSR = open("HighScore.txt", "r")
+    InterPTS = HSR.read()
+    InterPTS = int(InterPTS)
+    HSR.close()
+    print('Current High Score:', InterPTS, "\n")
 
-  sleep(0.5)
+    if InterPTS <= pts:
+        HS = open("HighScore.txt", "w")
+        StringPTS = str(pts)
+        HS.write(StringPTS)
+        HS.close()
+    else:
+        return 0
+
+def Question():
+    global lib
+    global pts
+    if realOutput == 1:
+        Question1 = (randint(1,1000))
+        st.write("output the value of x as:", Question1)
+        Writing()
+    elif realOutput == 2:
+        QuestionInput =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
+        st.write("Make a program that combines all these numbers, saved as x, y, z:", QuestionInput)
+        Writing()
+
+    sleep(0.5)
 
 def Checker():
-  global lib
-  global pts
-  global TrueUserInput
-  global Question1
-  global QuestionInput
-  if realOutput == 1:
-    if TrueUserInput.x == Question1:
-      print("\nCorrect!\n")
-      print("P:", pts, "+ 100")
-      pts += 100
-      sleep(1)
-    else:
-      print("\nIncorrect!\n")
-      print("P:", pts)
-      sleep(1)
-  elif realOutput==2:
-    if TrueUserInput.x+TrueUserInput.y+TrueUserInput.z == QuestionInput[0]+QuestionInput[1]+QuestionInput[2]:
-      print("\nCorrect\n")
-      print("P:", pts, "+ 150")
-      pts += 150
-      sleep(1)
-    else:
-      print("\nIncorrect!\n")
-      print("P:", pts)
-      sleep(1) 
+    global lib
+    global pts
+    global Question1
+    global QuestionInput
+    if realOutput == 1:
+        if state.TrueUserInput.x == Question1:
+            print("\nCorrect!\n")
+            print("P:", pts, "+ 100")
+            pts += 100
+            sleep(1)
+        else:
+            print("\nIncorrect!\n")
+            print("P:", pts)
+            sleep(1)
+    elif realOutput == 2:
+        if state.TrueUserInput.x + state.TrueUserInput.y + state.TrueUserInput.z == QuestionInput[0] + QuestionInput[1] + QuestionInput[2]:
+            print("\nCorrect\n")
+            print("P:", pts, "+ 150")
+            pts += 150
+            sleep(1)
+        else:
+            print("\nIncorrect!\n")
+            print("P:", pts)
+            sleep(1)
 
 st.title("Code-It-Out!!")
 st.divider()
