0cc574b3cbde4d9419915ad6427c72214f459ef4
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-06-07 13:15:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-import os
 import streamlit as st
 from random import randint
 from time import sleep
@@ -12,67 +11,60 @@ def Writing():
     TrueUserInput = st.text_area("Code: ")
     if st.button("Submit"):
         st.code(TrueUserInput)
-        script_path = os.path.dirname(os.path.abspath(__file__))
-        file_path = os.path.join(script_path, "TrueUserInput.py")
-        with open(file_path, 'w') as file_obj:
-            file_obj.write("code = '''\n")
-            file_obj.write(TrueUserInput)
-            file_obj.write("\n'''")
+        with open('TrueUserInput.py', 'w') as file_obj:
+            file_obj.write("x = {}\n".format(TrueUserInput))
 
 def HighScoreRecord():
-    global pts
-    HSR = open("HighScore.txt", "r")
-    InterPTS = HSR.read()
-    InterPTS = int(InterPTS)
-    HSR.close()
-    st.write('Current High Score:', InterPTS)
-
-    if InterPTS <= pts:
-        HS = open("HighScore.txt", "w")
-        StringPTS = str(pts)
-        HS.write(StringPTS)
-        HS.close()
-    else:
-        return 0
-
+  global pts
+  HSR = open("HighScore.txt", "r")
+  InterPTS = HSR.read()
+  InterPTS = int(InterPTS)
+  HSR.close()
+  print('Current High Score:', InterPTS, "\n")
+  
+  if InterPTS <= pts:
+    HS = open("HighScore.txt", "w")
+    StringPTS = str(pts)
+    HS.write(StringPTS)
+    HS.close()
+  else:
+    return 0
+  
 def Question():
-    global pts
-    if realOutput == 1:
-        Question1 = randint(1, 1000)
-        st.write("Output the value of x as:", Question1)
-        Writing()
+  global lib
+  global pts
+  global TrueUserInput
+  if realOutput == 1:
+    Question1 = (randint(1,1000))
+    st.write("output the value of x as:", Question1)
+    Writing()
+    
+  elif realOutput == 2:
+    QuestionInput =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
+    st.write("Make a program that combines all these numbers, saved as x, y, z:", QuestionInput)
+    Writing()
 
-        if st.button("Check Answer"):
-            if TrueUserInput == str(Question1):
+  sleep(0.5)
+
+def Checker():
+    global pts
+    global TrueUserInput
+    if realOutput == 1:
+        if TrueUserInput == str(Question1):
+            st.write("Correct!")
+            pts += 100
+        else:
+            st.write("Incorrect!")
+    elif realOutput == 2:
+        if 'x' in globals():
+            x_value = globals()['x']
+            if x_value == sum(QuestionInput):
                 st.write("Correct!")
-                pts += 100
+                pts += 150
             else:
                 st.write("Incorrect!")
-
-    elif realOutput == 2:
-        QuestionInput = [randint(0, 999), randint(1, 999), randint(1, 999)]
-        st.write("Make a program that combines all these numbers, saved as x, y, z:", QuestionInput)
-        Writing()
-
-        if st.button("Check Answer"):
-            st.code(TrueUserInput)
-            with open('TrueUserInput.py', 'w') as file_obj:
-                file_obj.write("x = {}\n".format(QuestionInput[0]))
-                file_obj.write("y = {}\n".format(QuestionInput[1]))
-                file_obj.write("z = {}\n".format(QuestionInput[2]))
-
-            try:
-                exec(TrueUserInput)
-                if 'x' in globals() and 'y' in globals() and 'z' in globals():
-                    if x + y + z == sum(QuestionInput):
-                        st.write("Correct!")
-                        pts += 150
-                    else:
-                        st.write("Incorrect!")
-                else:
-                    st.write("Variables x, y, z are not defined.")
-            except Exception as e:
-                st.write("Error:", str(e))
+        else:
+            st.write("Variable x is not defined.")
 
 st.title("Code-It-Out!!")
 st.divider()
