c174fe157cc98c4fac03c980d12b57f814904d9b
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-06-08 20:50:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,38 @@
 import streamlit as st
+from random import randint
+from importlib import reload
+from os import system
+from time import sleep
+l = 0
+pts = 0
+lib = 0
 
-code = st.text_area("Code: ")
+st.title("CODE-IT-OUT!!")
+st.divider()
 
-if st.button("Run"):
-  exec(code)
-  st.write("Why is:", x)
+def Question():
+  global lib
+  global pts
+  global TrueUserInput
+  if realOutput == 1:
+    Question1 = (randint(1,1000))
+    st.write("output the value of x as:", Question1)
+    code = st.text_area("Code: ")
+    
+  elif realOutput == 2:
+    QuestionInput =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
+    st.write("Make a program that combines all these numbers,saved as x, y, z:", QuestionInput)
+    code = st.text_area("Code: ")
+
+while l<1:
+  HighScoreRecord()
+  system('clear')
+  st.caption("P:", pts)
+  realOutput = (randint(1,2))
+  Question()
+  
+  if st.button("Run"):
+    exec(code)
+    st.write("Why is:", x)
+
+ 
