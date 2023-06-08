55142d4fcc6632e06a05b64adde0302e025947f3
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-06-08 21:20:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ from random import randint
 from time import sleep
 QuestionChoices = randint(1,2)
 pts = 0
-t = 0
 
 st.title("CODE-IT-OUT!!")
 st.write("Points:", pts)
@@ -11,15 +10,14 @@ st.divider()
 
 sleep(1)
 
-if t == 0:
-  if QuestionChoices == 1:
-    Question1 = (randint(1,1000))
-    st.write("output the value of x as:", Question1)
-  elif QuestionChoices == 2:
-    Question2 =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
-    st.write("Make a program that combines all these numbers,saved as x, y, z:", Question2)
-    t=1
+if QuestionChoices == 1:
+  Question1 = (randint(1,1000))
+  st.write("output the value of x as:", Question1)
+elif QuestionChoices == 2:
+  Question2 =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
+  st.write("Make a program that combines all these numbers,saved as x, y, z:", Question2)
 
+  
 code = st.text_area("Code: ")
 
 if st.button("Run"):
@@ -39,12 +37,3 @@ if st.button("Run"):
       pts+=100
     else:
       st.write("Wrong")
-
-if t==1:
-  if QuestionChoices == 1:
-    Question1 = (randint(1,1000))
-    st.write("output the value of x as:", Question1)
-  elif QuestionChoices == 2:
-    Question2 =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
-    st.write("Make a program that combines all these numbers,saved as x, y, z:", Question2)
-    t=1
