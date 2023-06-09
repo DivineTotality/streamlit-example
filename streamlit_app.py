12ad3a48637ac524b02105429c5b3838b62af3ea
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-06-09 21:10:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if 'result' not in st.session_state:
 
 if submit:
     if QuestionChoices == 1:
-        exec(code, globals(), locals())
+        with st.cache():
+            exec(code, globals(), locals())
         if 'x' in locals():
             st.code(x)
             if x == Question1:
@@ -47,7 +48,8 @@ if submit:
         else:
             st.write("Variable 'x' not defined")
     elif QuestionChoices == 2:
-        exec(code, globals(), locals())
+        with st.cache():
+            exec(code, globals(), locals())
         if 'x' in locals() and 'y' in locals() and 'z' in locals():
             st.code(x + y + z)
             if x + y + z == sum(Question2):
