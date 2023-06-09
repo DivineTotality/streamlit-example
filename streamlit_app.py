d98679f0efe590b09f436609a6a35ebab01c3974
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-06-09 21:42:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,41 +6,36 @@ st.title("CODE-IT-OUT!!")
 if 'pts' not in st.session_state:
     st.session_state['pts'] = 0
 
-question_choices = randint(1, 2)
-question1 = 0
-question2 = []
-
 def generate_question():
-    global question1
-    global question2
-    
+    question_choices = randint(1, 2)
+    question1 = 0
+    question2 = []
+
     if question_choices == 1:
         question1 = randint(1, 1000)
         st.write("Output the value of x as:", question1)
+        return question_choices, question1
     elif question_choices == 2:
         question2 = [randint(0, 999), randint(1, 999), randint(1, 999)]
         st.write("Make a program that combines all these numbers, saved as x, y, z:", question2)
+        return question_choices, question2
 
-def check_answer(code):
-    if question_choices == 1:
-        exec(code, globals())
-        if 'x' in globals() and x == question1:
-            return True
-        else:
-            return False
-    elif question_choices == 2:
-        exec(code, globals())
-        if 'x' in globals() and 'y' in globals() and 'z' in globals() and x + y + z == sum(question2):
-            return True
-        else:
-            return False
+question_choices, question = generate_question()
 
 form = st.form(key='my-form')
 code = form.text_area("Code:")
 submit = form.form_submit_button("Run")
 
 if submit:
-    result = check_answer(code)
+    result = False
+    if question_choices == 1:
+        exec(code, globals())
+        if 'x' in globals() and x == question:
+            result = True
+    elif question_choices == 2:
+        exec(code, globals())
+        if 'x' in globals() and 'y' in globals() and 'z' in globals() and x + y + z == sum(question):
+            result = True
     
     if result:
         st.write("**Correct!!**")
@@ -49,8 +44,7 @@ if submit:
         st.write("**Wrong**")
 
     # Generate a new question
-    question_choices = randint(1, 2)
-    generate_question()
+    question_choices, question = generate_question()
 
 st.write("Points:", st.session_state['pts'])
 st.divider()
