2a87e9ede165904eed2b798b4ee4498bea93c886
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-06-09 19:59:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ elif QuestionChoices == 2:
   
 code = st.text_area("Code: ")
 
-if st.button("Run"):
+if st.button("Run", refresh=False):
   if QuestionChoices == 1:
     exec(code)
     st.code(x)
@@ -37,3 +37,5 @@ if st.button("Run"):
       pts+=100
     else:
       st.write("Wrong")
+    sleep(1)
+    st.experimental_rerun()
