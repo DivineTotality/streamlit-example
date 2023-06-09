2d51d2a40be182ca623e240b2fde5951eeb0787f
DivineTotality <109021068+DivineTotality@users.noreply.github.com>
2023-06-09 20:45:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,21 @@
 import streamlit as st
 from random import randint
 from time import sleep
-QuestionChoices = randint(1,2)
+
+QuestionChoices = randint(1, 2)
 pts = 0
 Question1 = 0
-Question2 = {1, 2, 3}
+Question2 = [1, 2, 3]
 
 def Question(QuestionChoices):
-  global Question1
-  global Question2
-  if QuestionChoices == 1:
-    Question1 = (randint(1,1000))
-    st.write("output the value of x as:", Question1)
-  elif QuestionChoices == 2:
-    Question2 =[(randint(0,999)), (randint(1,999)), (randint(1,999))]
-    st.write("Make a program that combines all these numbers,saved as x, y, z:", Question2)
+    global Question1
+    global Question2
+    if QuestionChoices == 1:
+        Question1 = randint(1, 1000)
+        st.write("output the value of x as:", Question1)
+    elif QuestionChoices == 2:
+        Question2 = [randint(0, 999), randint(1, 999), randint(1, 999)]
+        st.write("Make a program that combines all these numbers, saved as x, y, z:", Question2)
 
 st.title("CODE-IT-OUT!!")
 st.write("Points:", pts)
@@ -29,19 +30,19 @@ code = form.text_area("Code:")
 submit = form.form_submit_button("Run")
 
 if submit:
-  if QuestionChoices == 1:
-    exec(code)
-    st.code(x)
-    if x == Question1:
-      st.write("Correct!!")
-      pts+=100
-    else:
-      st.write("Wrong")
-  elif QuestionChoices == 2:
-    exec(code)
-    st.code(x+y+z)
-    if x+y+z == Question2[0] + Question2[1] + Question2[2]:
-      st.write("Correct!!")
-      pts+=100
-    else:
-      st.write("Wrong")
+    if QuestionChoices == 1:
+        exec(code)
+        st.code(x)
+        if x == Question1:
+            st.write("Correct!!")
+            pts += 100
+        else:
+            st.write("Wrong")
+    elif QuestionChoices == 2:
+        exec(code)
+        st.code(x + y + z)
+        if x + y + z == sum(Question2):
+            st.write("Correct!!")
+            pts += 100
+        else:
+            st.write("Wrong")
